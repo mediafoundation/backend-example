e5aa0ecabbe2e9844efd489a12b1cfb47082fb1b
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-04-25 11:27:36 -0300

    feat(events): Get and store timestamp and provider of the event

--- database/controllers/eventsController.ts
+++ database/controllers/eventsController.ts
@@ -2,10 +2,23 @@ import {eventsCollection} from "../database"
 import {EventFormatted} from "../models/types/event"
 import {Document, Filter} from "mongodb"
 import {generateUniqueEventId} from "../../utils/hash"
+import {DealsController} from "./dealsController"
+import { Blockchain } from "media-sdk"
 
 export class EventsController {
-  static async upsertEvent(event: EventFormatted) {
-    await eventsCollection.insertOne(event)
+  static async upsertEvent(event: EventFormatted, chainId: number, blockChainInstance: Blockchain) {
+    // Make sure event belongs to marketplace
+    if(event.args._marketplaceId != process.env.MARKETPLACE_ID) {
+      throw new Error(`Event does not belong to marketplace ${process.env.MARKETPLACE_ID}`)
+    }
+
+    //Get deal-event provider
+    const deal = await DealsController.getDealByIdAndChain(event.args._dealId, chainId)
+
+    //Get block-event timestamp
+    const block = await blockChainInstance.getBlockTimestamp(BigInt(event.blockNumber))
+
+    await eventsCollection.insertOne({...event, provider: deal?.deal.provider, timestamp: block.timestamp})
   }
 
   static async getEvents(filter: Filter<Document> = {}) {
@@ -24,6 +37,7 @@ export class EventsController {
         result[key] = this.formatEvent(result[key])
       }
     }
+
     if(event.transactionHash) {
       result["_id"] = generateUniqueEventId(event.transactionHash)
     }
--- test/database/eventsController.test.ts
+++ test/database/eventsController.test.ts
@@ -2,6 +2,10 @@ import {connectToMongodb, eventsCollection} from "../../database/database"
 import {MongoClient} from "mongodb"
 import {EventsController} from "../../database/controllers/eventsController"
 import {generateUniqueEventId} from "../../utils/hash"
+import {Chain} from "../../database/models/Chain"
+import {resetDB} from "../../database/utils"
+import {DealsController} from "../../database/controllers/dealsController"
+import {Blockchain, Sdk, validChains} from "../../../media-sdk"
 
 let db: MongoClient
 const mockEvent = {
@@ -10,15 +14,46 @@ const mockEvent = {
   transactionHash: "0x62f395c3a1dd2daabc13d5b9663d8a36b42cecf380b0371252d59fcbf147b701",
   args: {
     _marketplaceId: 1n,
-    _dealId: 52n,
+    _dealId: 1n,
     _paymentToProvider: 10900000000118592000n,
     _totalPayment: 10900000000118592000n
   },
   eventName: "DealCreated"
 }
+const mockDeal = {
+  id: 1n,
+  offerId: 1n,
+  client: "0xB76c9A2fC1367f92cBB73b1ECE0c98Abb0c5097B",
+  provider: "0x2C0BE604Bd7969162aA72f23dA18634a77aFBB31",
+  resourceId: 3n,
+  totalPayment: 0n,
+  blockedBalance: 446499999999553500n,
+  terms: {
+    pricePerSecond: 111111111111n,
+    minDealDuration: 900n,
+    billFullPeriods: false,
+    singlePeriodOnly: false,
+    metadata: "{\"type\":\"cdn\",\"label\":\"Testing Backend\",\"apiEndpoint\":\"http:localhost:5000/\",\"bandwidthLimit\":{\"amount\":1,\"unit\":\"tb\",\"period\":\"monthly\"},\"autoSsl\":true,\"burstSpeed\":1000,\"nodeLocations\":[\"ABB\", \"CL\", \"BR\"],\"customCnames\":true}"
+  },
+  status: {
+    active: true,
+    createdAt: 1710153976n,
+    acceptedAt: 1710153976n,
+    billingStart: 1710153976n,
+    cancelled: false,
+    cancelledAt: 0n
+  }
+}
 
 beforeAll(async () => {
   db = await connectToMongodb()
+  await resetDB()
+  await Chain.create({
+    chainId: 1,
+    name: "Chain 1"
+  })
+
+  await DealsController.upsertDeal(DealsController.formatDeal(mockDeal), 1)
 })
 
 afterAll(async () => {
@@ -41,10 +76,12 @@ describe("Events Controller", () => {
 
   test("Insert event", async () => {
     const formattedEvent = EventsController.formatEvent(mockEvent)
-    await EventsController.upsertEvent(formattedEvent)
+    const sdk = new Sdk({chain: validChains["84532"]})
+    const blockchain = new Blockchain(sdk)
+    await EventsController.upsertEvent(formattedEvent, 1, blockchain)
     const events = await EventsController.getEvents()
     expect(events.length).toBe(1)
-    expect(events[0]).toStrictEqual(formattedEvent)
+    expect(events[0]).toStrictEqual({...formattedEvent, provider: mockDeal.provider, timestamp: 1706505070})
   })
 
   /*test("Calculate prover revenue", async () => {
