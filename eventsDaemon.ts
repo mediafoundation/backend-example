b6011025d400a33747419c15d67a4deabd5fbdf1
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-04-28 18:45:29 -0300

    feat(api.ts): Add endpoint to get new deals count of provider

--- eventsDaemon.ts
+++ eventsDaemon.ts
@@ -3,35 +3,41 @@ import {lastReadBlockCollection} from "./database/database"
 import {EventsController} from "./database/controllers/eventsController"
 import {createRelationsBetweenTables} from "./database/utils"
 
+const BATCH_SIZE = 1000n
+
 function delay(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) )
 }
 
-async function getEventsByBatches(eventsHandler: EventsHandler, blockChain: Blockchain, chainId: number) {
+async function getPastEvents(eventsHandler: EventsHandler, blockChain: Blockchain, chainId: number) {
   const lastBlockOnDb = await lastReadBlockCollection.findOne()
   let blockToRead = lastBlockOnDb ? BigInt(lastBlockOnDb.block) : 5284652n
 
   const currentBlock = await blockChain.getBlockNumber()
 
+  console.log("Current block", currentBlock)
+
   //await lastReadBlockCollection.insertOne(currentBlock)
 
   while (blockToRead < currentBlock) {
     try {
-      const events = await eventsHandler.getMarketplacePastEvents({eventName: undefined, fromBlock: blockToRead, toBlock: blockToRead + 1000n})
-      if(events.length !== 0){
-        console.log(events)
-      }
-      blockToRead = blockToRead + 1000n
+      const events = await eventsHandler.getMarketplacePastEvents({eventName: "DealCreated", fromBlock: blockToRead, toBlock: blockToRead + BATCH_SIZE})
+
       for (const event of events) {
         const blockTimestamp = await blockChain.getBlockTimestamp(event.blockNumber)
         await EventsController.upsertEvent(EventsController.formatEvent(event), chainId, Number(blockTimestamp.timestamp))
       }
+
+      await lastReadBlockCollection.updateOne({block: blockToRead}, {$set: {block: blockToRead + BATCH_SIZE}}, {upsert: true})
+      blockToRead = blockToRead + BATCH_SIZE
     } catch (e) {
       console.log(e)
     }
 
     await delay(1000)
   }
+
+  console.log("Finish")
 }
 
 async function start() {
@@ -47,7 +53,7 @@ async function start() {
     }*/
 
     const sdk = new Sdk({chain: validChains["11155111"]})
-    await getEventsByBatches(new EventsHandler(sdk), new Blockchain(sdk), 11155111)
+    await getPastEvents(new EventsHandler(sdk), new Blockchain(sdk), 11155111)
 
   } catch (e) {
     console.log("Error", e)
