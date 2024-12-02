6541c153b8eb46b3c9de684e7b2184852a26b5c8
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-12-02 13:39:00 -0300

    refactor(eventsListener.ts): Refactor functionality and make a safety reconnect every one hour

--- eventsListener.ts
+++ eventsListener.ts
@@ -1,135 +1,76 @@
-import {wssNetworks} from "./networks"
-import {Blockchain, EventsHandler, Marketplace, Sdk, validChains, webSocket} from "media-sdk"
+import { wssNetworks } from "./networks"
+import { Blockchain, EventsHandler, Marketplace, Sdk, validChains, webSocket } from "media-sdk"
 import EventsUtils from "./utils/events"
-import {createRelationsBetweenTables} from "./database/utils"
+import { createRelationsBetweenTables } from "./database/utils"
 
-createRelationsBetweenTables()
-  .then(() => {
-    console.log("Db Connected")
-    const chains: any[] = Object.values(validChains)
-    for (const chain of chains) {
-      const sdk = new Sdk({
-        chain: chain, transport: [webSocket(wssNetworks[chain.name][0], {
-          reconnect: {
-            delay: 1000,
-            attempts: 10
-          },
-          keepAlive: {
-            interval: 10000
-          },
-          timeout: 10000,
-          retryCount: 5,
-          retryDelay: 10000
-        })]
-      })
-      const blockchain = new Blockchain(sdk)
-      const marketplace = new Marketplace(sdk)
-      const eventsListener = new EventsHandler(sdk)
+const RECONNECT_INTERVAL = 3600000 // 1 hour in milliseconds
 
-      /*eventsListener.listenForResourcesEvent({
-        eventName: "AddedResource",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          await ResourcesController.upsertResource(ResourcesController.upsertResource())
-        }
-      })
-        .then(() => {
-        })*/
+async function initializeListeners() {
+  await createRelationsBetweenTables()
+  console.log("Db Connected")
 
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "DealCreated",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageDealUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
+  const chains: any[] = Object.values(validChains)
+  for (const chain of chains) {
+    let sdk = createSdk(chain)
+    let blockchain = new Blockchain(sdk)
+    let marketplace = new Marketplace(sdk)
+    let eventsListener = new EventsHandler(sdk)
 
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "DealAccepted",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageDealUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
+    setupEventListeners(eventsListener, marketplace, blockchain, chain.id)
 
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "DealCancelled",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageDealUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
+    setInterval(() => {
+      console.log(`Reconnecting to ${chain.name}...`)
+      sdk = createSdk(chain)
+      blockchain = new Blockchain(sdk)
+      marketplace = new Marketplace(sdk)
+      eventsListener = new EventsHandler(sdk)
+      setupEventListeners(eventsListener, marketplace, blockchain, chain.id)
+    }, RECONNECT_INTERVAL)
+  }
+}
 
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "OfferCreated",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageOfferUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
-
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "OfferUpdated",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageOfferUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
-
-      eventsListener.listenForMarketplaceEvent({
-        eventName: "OfferDeleted",
-        onError: (error: any) => {
-          console.log(error)
-        },
-        callback: async (event: any) => {
-          console.log(event)
-          for (const eventElement of event) {
-            await EventsUtils.manageOfferUpdated(eventElement, marketplace, blockchain, chain.id)
-          }
-        }
-      })
-        .then(() => {
-        })
-
-
-    }
+function createSdk(chain: any) {
+  return new Sdk({
+    chain: chain,
+    transport: [webSocket(wssNetworks[chain.name][0], {
+      reconnect: {
+        delay: 500, // Reduce delay between reconnection attempts
+        attempts: 20 // Increase the number of reconnection attempts
+      },
+      keepAlive: {
+        interval: 5000 // Send keep-alive messages more frequently
+      },
+      timeout: 5000, // Reduce the timeout duration
+      retryCount: 10, // Increase the number of retries
+      retryDelay: 5000 // Reduce the delay between retries
+    })]
   })
+}
 
+function setupEventListeners(eventsListener: EventsHandler, marketplace: Marketplace, blockchain: Blockchain, chainId: string) {
+  const events = [
+    { name: "DealCreated", handler: EventsUtils.manageDealUpdated },
+    { name: "DealAccepted", handler: EventsUtils.manageDealUpdated },
+    { name: "DealCancelled", handler: EventsUtils.manageDealUpdated },
+    { name: "OfferCreated", handler: EventsUtils.manageOfferUpdated },
+    { name: "OfferUpdated", handler: EventsUtils.manageOfferUpdated },
+    { name: "OfferDeleted", handler: EventsUtils.manageOfferUpdated }
+  ]
 
+  for (const event of events) {
+    eventsListener.listenForMarketplaceEvent({
+      eventName: event.name,
+      onError: (error: any) => {
+        console.log(error)
+      },
+      callback: async (event: any) => {
+        console.log(event)
+        for (const eventElement of event) {
+          await event.handler(eventElement, marketplace, blockchain, chainId)
+        }
+      }
+    }).then(() => {})
+  }
+}
+
+initializeListeners()
